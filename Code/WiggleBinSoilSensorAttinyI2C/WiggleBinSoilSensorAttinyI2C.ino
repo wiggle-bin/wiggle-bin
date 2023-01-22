651451b6f03464c9c9bf4ab0483037fe7e9bdd31
Vincent Kranendonk <git@studiorabota.com>
2023-01-22 17:26:05 +0100

    Added code for WiggleBinSoilSensor to measure moist and temperature of soil and send via I2C

---
+++ Code/WiggleBinSoilSensorAttinyI2C/WiggleBinSoilSensorAttinyI2C.ino
@@ -0,0 +1,49 @@
+#include <TinyWireS.h>
+#include <microDS18B20.h>
+
+typedef struct {
+  byte soilMoist;
+  float soilTemp;
+} SoilSensorData_t;
+
+typedef union I2C_Packet_t{
+ SoilSensorData_t sensor;
+ byte I2CPacket[sizeof(SoilSensorData_t)];
+};
+
+#define PACKET_SIZE 5
+
+I2C_Packet_t soilSensor;  
+
+const int AirValue = 880;
+const int WaterValue = 480;
+int soilMoistureValue = 0;
+int soilMoisturePercent = 0;
+
+MicroDS18B20<3> DS18B20sensor;
+
+void setup() {  
+  TinyWireS.begin(8);                
+  TinyWireS.onRequest(requestEvent);
+}
+
+void loop() {
+}
+
+void requestEvent()
+{
+  // Soil Moist
+  soilMoistureValue = analogRead(A2); 
+  soilMoisturePercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+  soilSensor.sensor.soilMoist = soilMoisturePercent; 
+
+  // Soil Temperature
+  DS18B20sensor.requestTemp();
+  delay(1000);
+  soilSensor.sensor.soilTemp = DS18B20sensor.getTemp();
+
+  // Send data over I2C
+  for (int k=0; k < PACKET_SIZE; k++) {
+    TinyWireS.send(soilSensor.I2CPacket[k]);
+  }
+}
