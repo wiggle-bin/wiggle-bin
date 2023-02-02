1c5aaf97838526834eb127307fce22fa1a2bae87
Vincent Kranendonk <git@studiorabota.com>
2023-02-02 22:36:18 +0100

    Send both temp and moist value from Soil Sensor component

--- Code/EspHome/WiggleBinSoilSensorComponent.h
+++ Code/EspHome/WiggleBinSoilSensorComponent.h
@@ -2,67 +2,56 @@
 
 using namespace esphome;
 
-bool getSoilData(SoilSensorData_t* soilSensor);
-
 class WiggleBinSoilSensor : public PollingComponent, public Sensor {
  public:
+  WiggleBinSoilSensor() : PollingComponent(15000) {}
+
+  Sensor *temperature_sensor = new Sensor();
+  Sensor *moisture_sensor = new Sensor();
+
+  float get_setup_priority() const override { return esphome::setup_priority::BUS; }
+
   typedef struct {
     byte soilMoist;
     float soilTemp;
   } SoilSensorData_t;
 
-  typedef union FloatToBytes_t 
+  union FloatToBytes_t 
   { 
     float f; 
     char b[sizeof(float)]; 
   };
 
   #define PACKET_SIZE 5
-
-  WiggleBinSoilSensor() : PollingComponent(15000) {}
-
-  float get_setup_priority() const override { return esphome::setup_priority::BUS; }
-
+  #define soilSensordResponderAddress 8
+  
   void setup() override {
     Wire.begin();
   }
 
-  void write_state(float state) override {
+  void update() override {
     SoilSensorData_t soilSensor; 
-    if(getSoilData( &soilSensor )) {
-        publish_state(soilSensor.soilMoist);
+    
+    byte index = 0;
+    byte I2C_Packet[PACKET_SIZE];
+
+    Wire.requestFrom(soilSensordResponderAddress, PACKET_SIZE);
+    
+    while (Wire.available()){
+      I2C_Packet[index++] = Wire.read();
     }
+
+    soilSensor.soilMoist = I2C_Packet[0];
+    
+    FloatToBytes_t soilTemp;
+    soilTemp.b[0] = I2C_Packet[1];
+    soilTemp.b[1] = I2C_Packet[2];
+    soilTemp.b[2] = I2C_Packet[3];
+    soilTemp.b[3] = I2C_Packet[4];
+  
+    soilSensor.soilTemp = soilTemp.f;
+
+    temperature_sensor->publish_state(soilSensor.soilTemp);
+    moisture_sensor->publish_state(soilSensor.soilMoist);
   }
 };
-
-bool getSoilData(SoilSensorData_t* soilSensor)
-{
-  bool gotI2CPacket = false;  
-  byte index = 0;
-  byte I2C_Packet[PACKET_SIZE];
-
-  Wire.requestFrom(soilSensordResponderAddress, PACKET_SIZE);
-  
-  while (Wire.available()){
-      I2C_Packet[index++] = Wire.read();
-      gotI2CPacket = true;
-  }
-
-  if(gotI2CPacket) {
-      gotI2CPacket = false;
-
-      soilSensor->soilMoist = I2C_Packet[0];
-      
-      FloatToBytes_t soilTemp;
-      soilTemp.b[0] = I2C_Packet[1];
-      soilTemp.b[1] = I2C_Packet[2];
-      soilTemp.b[2] = I2C_Packet[3];
-      soilTemp.b[3] = I2C_Packet[4];
-    
-      soilSensor->soilTemp = soilTemp.f;
-
-      return true;
-   } else { 
-      return false; 
-   }
-}
