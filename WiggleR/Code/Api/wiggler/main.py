6e5b1726637fbc71ca0dc31f492f9dd466bf3a27
Vincent Kranendonk <git@studiorabota.com>
2023-07-15 11:54:36 +0200

    Added endpoint to view image

--- WiggleR/Code/Api/wiggler/main.py
+++ WiggleR/Code/Api/wiggler/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from datetime import datetime
 import os
 
+IMG_FOLDER = './Pictures'
+
 app = FastAPI()
+
+app.mount("/image", StaticFiles(directory=IMG_FOLDER), name="image")
 
 @app.get("/")
 def read_root():
@@ -11,19 +16,19 @@ def read_root():
 @app.get("/images")
 def images():
     out = []
-    for filename in sorted(os.listdir("./images")):
-        name, ext = os.path.splitext(filename)
+    for fileName in sorted(os.listdir(IMG_FOLDER)):
+        name, ext = os.path.splitext(fileName)
         if ext == '.jpg':
             out.append({
                 "name": name,
-                "path": "/image/" + filename
+                "path": "/image/" + fileName
             })
     return out
 
-@app.get("/picture")
+@app.get("/take_picture")
 def images():
     now = datetime.now()
-    filename = now.strftime("%Y-%m-%d-%H-%M")
-    path = f"./images/{filename}.jpg"
-    os.system(f"libcamera-jpeg --width 1024 --height 768 --nopreview -t 1 -o {path}")
-    return {"picture": path}
+    fileName = now.strftime("%Y-%m-%d-%H-%M")
+    filePath = f"{IMG_FOLDER}/{fileName}.jpg"
+    os.system(f"libcamera-jpeg --width 1024 --height 768 --nopreview -t 1 -o {filePath}")
+    return {"picture": f"image/{fileName}.jpg"}
