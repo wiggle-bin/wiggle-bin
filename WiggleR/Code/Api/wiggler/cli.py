74d8f5de24adfca73e1c677310179430b96b6449
Vincent Kranendonk <git@studiorabota.com>
2023-07-16 10:24:28 +0200

    Added command to start server on boot of Raspberry Pi

--- WiggleR/Code/Api/wiggler/cli.py
+++ WiggleR/Code/Api/wiggler/cli.py
@@ -1,13 +1,22 @@
 import argparse
 import os
+from pathlib import Path
 
 def main():
   
     parser = argparse.ArgumentParser(prog ='wiggler', description ='WiggleR API')
 
     parser.add_argument('-s', '--server', action='store_true')
+    parser.add_argument('-b', '--boot', action='store_true')
     
     args = parser.parse_args()
   
     if args.server:
         os.system(f"uvicorn wiggler.main:app --reload --host 0.0.0.0")
+
+    if args.boot:
+        scriptFile = Path(__file__).parent / f"service/wiggler_boot.sh"
+        serviceFile = Path(__file__).parent / f"service/wiggler.service"
+        os.system(f'sudo cp {scriptFile} /usr/bin/wiggler_boot.sh')
+        os.system(f'sudo cp {serviceFile} /etc/systemd/user/wiggler.service')
+        os.system('systemctl --user enable wiggler.service')
---
+++ WiggleR/Code/Api/wiggler/service/wiggler_boot.sh
@@ -0,0 +1,2 @@
+PATH=/home/$USER/.local/bin:$PATH
+wiggler -s
