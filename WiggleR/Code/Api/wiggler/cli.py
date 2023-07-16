1796d787408b16a0d08d8296a6b2b243fa8a08e0
Vincent Kranendonk <git@studiorabota.com>
2023-07-16 13:09:38 +0200

    Added commands to control wiggler service

--- WiggleR/Code/Api/wiggler/cli.py
+++ WiggleR/Code/Api/wiggler/cli.py
@@ -2,21 +2,40 @@ import argparse
 import os
 from pathlib import Path
 
-def main():
-  
-    parser = argparse.ArgumentParser(prog ='wiggler', description ='WiggleR API')
 
-    parser.add_argument('-s', '--server', action='store_true')
-    parser.add_argument('-b', '--boot', action='store_true')
-    
+def main():
+
+    parser = argparse.ArgumentParser(prog='wiggler', description='WiggleR API')
+
+    parser.add_argument('-s', '--server', action='store_true',
+                        help='start api server')
+
+    service = parser.add_argument_group("service")
+    service.add_argument('--service-install', 
+                         action='store_true',
+                         help='install wiggler service to start api server on boot')
+    service.add_argument('--service', 
+                         const='status', 
+                         nargs='?',
+                         choices=['stop', 'start',
+                                  'status', 'disable', 'enable'],
+                         help='control wiggler service')
+
     args = parser.parse_args()
-  
+
     if args.server:
         os.system(f"uvicorn wiggler.main:app --reload --host 0.0.0.0")
-
-    if args.boot:
+    elif args.service_install:
         scriptFile = Path(__file__).parent / f"service/wiggler_boot.sh"
         serviceFile = Path(__file__).parent / f"service/wiggler.service"
         os.system(f'sudo cp {scriptFile} /usr/bin/wiggler_boot.sh')
         os.system(f'sudo cp {serviceFile} /etc/systemd/user/wiggler.service')
         os.system('systemctl --user enable wiggler.service')
+        os.system('systemctl --user start wiggler.service')
+    elif args.service:
+        os.system(f'systemctl --user {args.service} wiggler.service')
+    else:
+        print("run wiggler -h for options")
+
+if __name__ == '__main__':
+    main()
